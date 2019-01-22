e73847bee52a51df6d0be10d8371e920de41f7ee
garciaa324 <garciaa324@ALB205-9.strose.edu>
2019-01-22 15:40:23 -0500

    creating hello

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.prinln()"Enter your name: ");
+		String name = keyboard.nextLine();
+		System.out.println("Hello " + name);
+		
+
+	}
+
+}
