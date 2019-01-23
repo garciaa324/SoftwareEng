ff8375c989662de478b6799e4b7b84b23a314dbb
garciaa324 <31697945+garciaa324@users.noreply.github.com>
2019-01-23 16:41:59 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		String name;
 		Scanner keyboard = new Scanner(System.in);
-		System.out.prinln()"Enter your name: ");
+		System.out.println("Enter your name: ");
 		name = keyboard.nextLine();
 		System.out.println("Hello " + name);
 		
