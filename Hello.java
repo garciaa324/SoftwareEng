29b9c1a1c484604517c209d82804c62b7eeffac9
garciaa324 <31697945+garciaa324@users.noreply.github.com>
2019-01-23 16:41:13 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,12 +1,11 @@
 import java.util.*;
 
 public class Hello {
-
 	public static void main(String[] args) {
-		
+		String name;
 		Scanner keyboard = new Scanner(System.in);
 		System.out.prinln()"Enter your name: ");
-		String name = keyboard.nextLine();
+		name = keyboard.nextLine();
 		System.out.println("Hello " + name);
 		
 
